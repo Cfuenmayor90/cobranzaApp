1060efd981119dfa52cf4a8c021fc410356cb4cb
cfuenmayor90 <cfuenmayor04@gmail.com>
2024-10-11 22:34:27 -0300

    s

--- middleware/timeOpe.js
+++ middleware/timeOpe.js
@@ -1,6 +1,6 @@
 const timeOp = (req,res,next) => {
     const hrFinal = "20:30:00";
-    const hrActual = new Date().toLocaleTimeString("es-AR", {timeZone: 'America/Argentina/Buenos_Aires'});
+    const hrActual = new Date().toLocaleTimeString("es-AR", {timeZone: 'America/Buenos_Aires'});
     const diaOp = new Date().getDay(); //obtenemos el numero de dia actual, si es domingo sera 0
     console.log(hrActual);
     console.log(hrFinal);
