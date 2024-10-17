f15a33c2bc120e45eab1ff6274ba01b3fec0d437
cfuenmayor90 <cfuenmayor04@gmail.com>
2024-10-16 23:44:04 -0300

    timeOp

--- middleware/timeOpe.js
+++ middleware/timeOpe.js
@@ -1,6 +1,6 @@
 const timeOp = (req,res,next) => {
     const hrFinal = "20:30:00";
-    const hrActual = new Date().toLocaleTimeString("es-AR", {timeZone: 'America/Buenos_Aires'});
+    const hrActual = new Date().toLocaleTimeString();
     const diaOp = new Date().getDay(); //obtenemos el numero de dia actual, si es domingo sera 0
     console.log(hrActual);
     console.log(hrFinal);
@@ -11,6 +11,6 @@ const timeOp = (req,res,next) => {
         const mensajeError = "¡No Puedes Registrar Pagos En Este Horario! Intentalo antes de 20:30:00";
         res.render('error', {mensajeError});
     }
-}
+};
 
 module.exports = {timeOp};
