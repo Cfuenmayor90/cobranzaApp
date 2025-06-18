81eda2d8f9f154878b824d61f1f2a1108fd59091
Cfuenmayor90 <137357380+Cfuenmayor90@users.noreply.github.com>
2025-06-18 10:24:29 -0300

    Add files via upload

--- routes/calculadoraRoutes.js
+++ routes/calculadoraRoutes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const {cargarCalculadora, calcular} = require('../controllers/calculadoraControllers');
 const {checkRole} = require('../controllers/usersControllers');
 
-router.get('/', checkRole(['admin', 'vendedor', 'cobrador']), cargarCalculadora);
-router.post('/calcular', checkRole(['admin', 'vendedor', 'cobrador']), calcular);
+router.get('/', checkRole(['admin', 'vendedor', 'cobrador', 'pisoDeVenta']), cargarCalculadora);
+router.post('/calcular', checkRole(['admin', 'vendedor', 'cobrador', 'pisoDeVenta']), calcular);
 module.exports = router;
