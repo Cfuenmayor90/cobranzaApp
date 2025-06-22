d2b5f912d9a62fd1b73b975b23be410698f6358e
Cfuenmayor90 <137357380+Cfuenmayor90@users.noreply.github.com>
2025-06-22 18:33:41 -0300

    ss

---
+++ generalRoutes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const { checkRole } = require('../controllers/usersControllers');
+const {cargarGeneral, guardarCaja, cargarPrestamosRuta, editCliente, cargarEstadisticas, deleteCajaOpe, load, cargarEstadoClient} = require('../controllers/generalControllers');
+
+router.get('/', checkRole(['admin']), cargarGeneral);
+router.post('/cajaOperacion', checkRole(['admin']), guardarCaja);
+router.get('/deleteCajaOperacion/:_id', checkRole(['admin']), deleteCajaOpe);
+router.get('/cargarPres/:nRuta', checkRole(['admin']), cargarPrestamosRuta);
+router.get('/statusPresClient/:dni', checkRole(['admin']), cargarEstadoClient);
+router.get('/editCliente/:dni', checkRole(['admin']), editCliente);
+router.get('/estadisticaUser/:nRuta', checkRole(['admin', 'cobrador', 'vendedor', 'pisoDeVenta']), cargarEstadisticas);
+router.post('/estadisticaUserFecha', checkRole(['admin', 'cobrador', 'vendedor', 'pisoDeVenta']), cargarEstadisticas);
+
+module.exports = router;
