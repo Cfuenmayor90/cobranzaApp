d69e3c724715471144435fba5a44e292a8d954ec
Cfuenmayor90 <137357380+Cfuenmayor90@users.noreply.github.com>
2025-08-27 18:17:22 -0300

    Add files via upload

--- routes/ventasRoutes.js
+++ routes/ventasRoutes.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 const {checkRole} = require('../controllers/usersControllers');
 const {timeOp} = require('../middleware/timeOpe');
-const {cotizarPlan, cargarVentas, guardarVentas, cargarVenCob, cotizarContado, guardarVentasContado} = require('../controllers/ventasControllers');
+const {preCotizar, preCotizarCont, cotizarPlan, cargarVentas, guardarVentas, cargarVenCob, cotizarContado, guardarVentasContado} = require('../controllers/ventasControllers');
 
 
 router.get('/', checkRole(['admin', 'pisoDeVenta']), cargarVentas);
-
+router.post('/preCotizar', checkRole(['admin', 'pisoDeVenta']), preCotizar );
+router.post('/preCotizarCont', checkRole(['admin', 'pisoDeVenta']), preCotizarCont );
 router.post('/cotizar', checkRole(['admin', 'pisoDeVenta']), cotizarPlan);
 router.post('/cotizarContado', checkRole(['admin', 'pisoDeVenta']), cotizarContado);
 router.post('/guardar', checkRole(['admin', 'pisoDeVenta']), guardarVentas);
