c71ea32b26a39d6a0e239a949e950856b2ffecce
Cfuenmayor90 <137357380+Cfuenmayor90@users.noreply.github.com>
2025-08-29 17:21:19 -0300

    Add files via upload

--- routes/generalRoutes.js
+++ routes/generalRoutes.js
@@ -7,7 +7,7 @@ router.get('/', checkRole(['admin']), cargarGeneral);
 router.post('/cajaOperacion', checkRole(['admin']), guardarCaja);
 router.get('/deleteCajaOperacion/:_id', checkRole(['admin']), deleteCajaOpe);
 router.get('/cargarPres/:nRuta', checkRole(['admin']), cargarPrestamosRuta);
-router.get('/statusPresClient/:dni', checkRole(['admin']), cargarEstadoClient);
+router.get('/statusPresClient/:dni', checkRole(['admin', 'pisoDeVenta']), cargarEstadoClient);
 router.get('/editCliente/:dni', checkRole(['admin']), editCliente);
 router.get('/estadisticaUser/:nRuta', checkRole(['admin', 'cobrador', 'vendedor', 'pisoDeVenta']), cargarEstadisticas);
 router.post('/estadisticaUserFecha', checkRole(['admin', 'cobrador', 'vendedor', 'pisoDeVenta']), cargarEstadisticas);
