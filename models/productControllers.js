89a84691cba7cb100f4db4924246ab23b3b84c77
Cfuenmayor90 <137357380+Cfuenmayor90@users.noreply.github.com>
2025-10-01 19:46:42 -0300

    transf

---
+++ models/productControllers.js
@@ -0,0 +1,173 @@
+const product = require('../models/productModels');
+const setting = require('../models/settingsModels'); //setting para ver los planes
+const settingValores = require('../models/settingValoresModels'); //sectin para valores de ganancia
+const multer = require('multer');
+const path = require('path');
+
+const f = new Intl.NumberFormat('es-AR', {
+    style: 'currency',
+    currency: 'ARS',
+    minimumFractionDigits: 2
+});
+const arrayCategoriasProd = [{valor:'Electrodomesticos'},{valor:'Infantil'}   ,{valor: 'Indumentaria'}, {valor: 'Calzado'}, {valor:'Hogar'}, {valor:'Equipamiento Comercial'}, {valor:'Electronica'}, {valor:'Celulares'}, {valor:'Accesorios Para Celulares'}, {valor:'Accesorios Para Vehiculos'}, {valor:'Herramientas' }, {valor:'Cables y Conectores' }];
+
+//pag de productos para ADMIN
+const cargarProducts = async(req, res) =>{
+    try {
+        const productos = await product.find().sort({nombre: 1});
+        const prodStock = await product.find({stock: {$gte: 1}});
+        var capiProd = 0;
+        var cantProdStock = 0; //cantidad de productos totales en stock
+        var cantProd = prodStock.length; //cantidad de productos
+        prodStock.forEach(element => {
+            var prodM = element.stock * element.precio;
+            capiProd = capiProd + prodM;
+            cantProdStock = cantProdStock + element.stock;
+        });
+        console.log("productos en estock  " + capiProd);
+        res.render('productos', {productos, arrayCategoriasProd, capiProd, cantProd, cantProdStock});
+        
+    } catch (error) {
+        
+    }
+};
+//pag de productos para usuarios y clientes
+const cargarPagProductos = async(req, res) =>{
+    try {
+        const productos = await product.find().sort({nombre: 1});
+        const cant = productos.length;
+        res.render('productosUsuarios', {productos , arrayCategoriasProd, cant});
+    } catch (error) {
+        
+    }
+};
+//imprimir hoja de productos
+const imprimirProd = async(req, res) =>{
+      try {
+        const productos = await product.find().sort({nombre: 1});
+        const cant = productos.length;
+        res.render('listProdPrint', {productos , arrayCategoriasProd, cant});
+    } catch (error) {
+        
+    }
+};
+const filtrarProd = async(req, res) =>{
+    try {
+        const {categoria} = req.params;
+        const productos = await product.find({categoria});
+        const cant = productos.length;
+        res.render('productosUsuarios', {productos , arrayCategoriasProd, cant});
+    } catch (error) {
+        
+    }
+}
+const cotizarProd = async(req, res) => {
+    try {
+        const {id} = req.params;
+        const prod = await product.findOne({_id: id});
+        const planesDiarios = await setting.find({categoria: "financiamiento",plan: "diario" }).sort({cuotas: 1});
+        const planesSemanales = await setting.find({categoria: "financiamiento",plan: "Semanal" }).sort({cuotas: 1});
+        const planesMensuales = await setting.find({categoria: "financiamiento",plan: "mensual" }).sort({cuotas: 1});
+        const valores = await settingValores.findOne();
+        var precio = (prod.precio * valores.dolar) * ((valores.porcentaje * 0.01) + 1);
+        var precioTartj = precio *((valores.tcredito * 0.01) + 1);
+        var vCuota = precioTartj / 3;
+        const arrayPlanes = [];
+        planesDiarios.forEach(element => {
+            var xcentaje = (element.porcentaje/100)+1;
+            var cuota = (precio*xcentaje)/element.cuotas;
+            var total = (element.cuotas * cuota).toFixed(2);
+            cuota = f.format(cuota)
+            arrayPlanes.push({"plan": element.plan, "cuotas": element.cuotas, "porcentaje": element.porcentaje, "cuota": cuota, "total": total });
+        });
+        planesSemanales.forEach(element => {
+            var xcentaje= (element.porcentaje/100)+1;
+            var cuota =(precio*xcentaje)/element.cuotas;
+            cuota = f.format(cuota);
+            arrayPlanes.push({"plan": element.plan, "cuotas": element.cuotas, "porcentaje": element.porcentaje, "cuota": cuota});
+    });
+    planesMensuales.forEach(element => {
+            var xcentaje= (element.porcentaje/100)+1;
+            var cuota =(precio*xcentaje)/element.cuotas;
+            cuota = f.format(cuota);
+            arrayPlanes.push({"plan": element.plan, "cuotas": element.cuotas, "porcentaje": element.porcentaje, "cuota": cuota});
+    });
+    
+       precio = f.format(precio);
+       vCuota = f.format(vCuota);
+       precioTartj = f.format(precioTartj);
+        res.render('cotizarProd', {prod, precio, precioTartj, vCuota, arrayPlanes})
+    } catch (error) {
+        
+    }
+}
+//midleware de MULTER   
+const storage = multer.diskStorage({
+    destination: path.join(__dirname, '../public/uploads'),
+    filename: (req, file, cb) =>{
+        cb(null, `${req.body.cod}.jpg`);
+    }
+
+});
+const upload = multer({
+    storage,
+    dest: path.join(__dirname, '../public/uploads'),
+    limits:{fileSize: 1000000},
+    fileFilter: (req, file, cb) =>{
+        const filetypes = /jpeg|jpg|png/;
+        const mimetype = filetypes.test(file.mimetype);
+        const extname = filetypes.test(path.extname(file.originalname));
+        if (mimetype && extname) {
+            return cb(null, true);
+        }
+        var mensajeError = "Error al cargar la IMAGEN, formatos aceptados: jpeg|jpg|png"
+        res.render('error', {mensajeError})
+    }
+    
+}).single('image');
+
+const saveProducts = async(req, res) =>{
+    try {
+        const newProduct = new product(req.body);
+        await newProduct.save()
+        res.send('todo ok');
+        
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+const prodEditGet = async(req, res) =>{
+    try {
+        const {_id} =  req.params;
+        const prod = await product.findById({_id});
+        console.log(prod);
+        res.render('productoEdit', {prod, arrayCategoriasProd });
+
+    } catch (error) {
+        
+    }
+};
+
+const prodEditSave = async(req, res) =>{
+    try {
+        const prodEdit = req.body;
+        const {id} = req.params;
+         await product.findByIdAndUpdate({_id:id}, prodEdit);
+        //console.log("producto a editar" + prodEdit);
+        res.redirect('/products/productos');
+    } catch (error) {
+        
+    }
+};
+const prodDelete = async(req, res) =>{
+    try {
+        const {id} = req.params;
+        await product.findByIdAndDelete({_id: id});
+        res.redirect('/products/productos');
+    } catch (error) {
+        
+    }
+}
+
+module.exports = {cargarProducts, cargarPagProductos, imprimirProd, upload, saveProducts, cotizarProd, filtrarProd, prodEditGet, prodEditSave, prodDelete };
---
+++ models/ventasControllers.js
@@ -0,0 +1,469 @@
+const users = require("../models/userModels");
+const setPrest = require('../models/settingsModels');
+const client = require("../models/clientModels");
+const ventas = require('../models/ventasModels');
+const historyVentas = require('../models/historyVentas');
+const product = require('../models/productModels');
+const balance = require('../models/balanceModels');
+const setValores = require('../models/settingValoresModels');
+const cajaOp = require('../models/cajaModels');
+const { generarJWT, verifyJWT} = require('../middleware/jwt');
+const f = new Intl.NumberFormat('es-AR', {
+    style: 'currency',
+    currency: 'ARS',
+    minimumFractionDigits: 2
+});
+
+const cargarVentas = async(req, res) => {
+    const fechaHoy = new Date().toLocaleDateString("es-AR", {timeZone: 'America/Argentina/Buenos_Aires'});
+     const usuariosVent = await users.find({role: ['cobrador','vendedor', 'pisoDeVenta']});
+     const usuariosCob = await users.find({role: ['cobrador', 'pisoDeVenta']});
+     const productos = await product.find().sort({nombre: 1});
+     const planPrest = await setPrest.find({categoria: 'prestamo'}).sort({porcentaje: 1});
+     const planProd = await setPrest.find({categoria: 'financiamiento'}).sort({porcentaje: 1});
+     const ventasT = await ventas.find().sort({timeStamp: -1}).limit(20);
+     const ventasCont = await historyVentas.find({categoria: "contado"}).sort({timeStamp: -1}).limit(20);
+     return res.render('ventas', {usuariosVent,usuariosCob, planPrest, planProd, ventasT, ventasCont, productos});
+}
+//cargar ventas totales en el mes
+const fecthVentasMes = async(req, res) =>{
+const {mesInp, anioInp} = req.body;
+    var anio = anioInp || new Date().getFullYear();
+    var mes = mesInp || new Date().getMonth();
+    var m = parseInt(mes) + 1;
+
+     
+     var cantDias = new Date(anio,m,0);
+     cantDias = cantDias.getDate();
+
+     const ventasFina = await historyVentas.find({categoria:"financiamiento", timeStamp:{$gte: new Date(anio,mes,1), $lte: new Date(anio,mes,cantDias,23,59,59,0)}}).sort({timeStamp: -1});
+    const ventasCont = await historyVentas.find({categoria: "contado",timeStamp:{$gte: new Date(anio,mes,1), $lte: new Date(anio,mes,cantDias,23,59,59,0)} }).sort({timeStamp: -1});
+    
+    res.render('historialVentas', {ventasFina, ventasCont, anio, mes: m});
+  }
+
+
+
+//cargar ventas especificas de usuarios como vendedore y cobradores
+const cargarVenCob = async(req, res) =>{
+  const {nRuta} = req.params;
+  res.send('ssss')
+}
+
+const preCotizar = async(req, res) =>{
+  try {
+     const cot = req.body;
+    console.log(cot);
+    var mensajeError = '';
+    const fecha = cot.fecha;
+    const anio = new Date(fecha).getFullYear();
+    const mes = new Date(fecha).getMonth();
+    const dia = new Date(fecha).getUTCDate();
+    const fechaVent = new Date(anio, mes, (dia + 1)).toLocaleDateString("es-AR", {timeZone: 'America/Argentina/Buenos_Aires'});
+    const cliente = await client.findOne({dni:cot.dni});
+    const balanCEDiario = await balance.findOne({cobRuta: cot.cobRuta, fecha: fechaVent});
+    console.log("fecha de venta " + cot.fecha);
+    console.log("balance encontrado " + balanCEDiario);
+    if (!cliente) {
+      mensajeError = '¡El Cliente NO existe en la DATA BASE!';
+      res.render('error', {mensajeError});
+    }
+     if (balanCEDiario) {
+       const arrayCod = [];
+         if (cot.codProd == "prestamo"){
+          const planCot = await setPrest.findById({_id:cot.planId});
+        const mTotal = (cot.monto * ((planCot.porcentaje / 100)+1)).toFixed(2);
+        const cuota = (mTotal / planCot.cuotas).toFixed(2);
+        arrayCod.push({"cod": cot.codProd, "cant": 1});
+        res.render('confirmarVenta', {cot, planCot, cuota, mTotal, cliente, arrayCod});
+         }
+         else{
+        const codProducts = cot.codProd;
+        if (Array.isArray(codProducts)) {
+          for (let i = 0; i < codProducts.length; i++) {
+            const element = codProducts[i];
+            console.log(element);
+            
+            var prod = await product.findOne({cod: element});
+            console.log("producto");
+            console.log(prod);
+            arrayCod.push({"cod":element,"nombre": prod.nombre});
+          };  
+          res.render('cantProdVenta', {cot,arrayCod, cliente});
+        }
+        else{
+          var prod = await product.findOne({cod: codProducts});
+          arrayCod.push({"cod":codProducts, "nombre": prod.nombre});
+          precio= prod.precio;
+          res.render('cantProdVenta', {cot,arrayCod, cliente});
+        }
+         }
+     }
+  } catch (error) {
+    
+  }
+}
+
+
+
+const cotizarPlan = async(req, res) => {
+  try {
+    const cot = req.body;
+    
+    var mensajeError = '';
+    const fecha = cot.fecha;
+    const anio = new Date(fecha).getFullYear();
+    const mes = new Date(fecha).getMonth();
+    const dia = new Date(fecha).getUTCDate();
+    const fechaVent = new Date(anio, mes, (dia + 1)).toLocaleDateString("es-AR", {timeZone: 'America/Argentina/Buenos_Aires'});
+    const cliente = await client.findOne({dni:cot.dni});
+    const balanCEDiario = await balance.findOne({cobRuta: cot.cobRuta, fecha: fechaVent});
+    console.log("fecha de venta " + cot.fecha);
+    console.log("balance encontrado " + balanCEDiario);
+    if (!cliente) {
+      mensajeError = '¡El Cliente NO existe en la DATA BASE!';
+      res.render('error', {mensajeError});
+    }
+    if (balanCEDiario) {
+      console.log("cotizar producto");
+      //array con codigos de productos o prestamo
+      const arrayCod = [];
+      console.log(cot);
+      if (cot.codProd == "prestamo") {
+        const planCot = await setPrest.findById({_id:cot.planId});
+        const mTotal = (cot.monto * ((planCot.porcentaje / 100)+1)).toFixed(2);
+        const cuota = (mTotal / planCot.cuotas).toFixed(2);
+        arrayCod.push({"cod": cot.codProd});
+        res.render('confirmarVenta', {cot, planCot, cuota, mTotal, cliente, arrayCod});
+      }
+      else{
+        
+        const valores = await setValores.findOne();
+        const codProducts = cot.codProd;
+        var precio = 0;
+        if (Array.isArray(codProducts)) {
+          console.log("is array");
+          
+          for (let i = 0; i < codProducts.length; i++) {
+            const element = codProducts[i];
+            
+            var prod = await product.findOne({cod: element});
+           var el = element;
+            
+            var cantProd = req.body[element];
+            console.log("elemento: " + cantProd);
+            
+            console.log("producto cantidad: " + cantProd);
+            console.log(prod);
+            precio = ((prod.precio) * cantProd) + precio;
+            console.log(precio);
+            arrayCod.push({"cod":element,"nombre": prod.nombre, "precio": prod.precio, "cant": cantProd, "Total": (prod.precio * cantProd)});
+            console.log("array productos " + arrayCod);
+          };  
+        }
+        else{
+          var prod = await product.findOne({cod: codProducts});
+          var cantProd = req.body[codProducts];
+          precio= prod.precio * cantProd;
+          arrayCod.push({"cod":codProducts, "nombre": prod.nombre, "precio": prod.precio, "cant": cantProd, "Total": (prod.precio * cantProd) });
+        }
+
+        
+        const precioT = (precio * valores.dolar) * ((valores.porcentaje / 100) +1);
+        const des = (cot.descuento / 100);
+    
+        const planCot = await setPrest.findById({_id:cot.planId});
+        const mTotal = ((precioT * ((planCot.porcentaje / 100)+1))-(des * (precioT * ((planCot.porcentaje / 100)+1)))).toFixed(2);
+        const cuota = (mTotal / planCot.cuotas).toFixed(2);
+        res.render('confirmarVenta', {cot, planCot, cuota, mTotal, cliente, arrayCod});
+        
+      }
+     }
+    else{
+      mensajeError = '¡La fecha de venta ingresada no es permitida!, elija una fecha que tenga un BALANCE DIARIO generado en esa ruta de cobro';
+      res.render('error', {mensajeError});
+    }
+    
+  } catch (error) {
+    res.render('error');
+  }
+};
+
+
+const guardarVentas = async(req,res) => {
+try {
+  //jwt para determinar el usuario que hace la operacion
+   const token =  req.cookies.token; // Obtener el token JWT de las cookies de la solicitud
+    const verifyToken = await verifyJWT(token);
+    const rol = verifyToken.role;
+    const nRuta = verifyToken.numRuta;
+    //.........................
+  const {fecha} = req.body;
+  const newVenta = new ventas(req.body);
+  const ade = newVenta.adelanto;
+  const cdp = req.body.codProd;
+  console.log("New venta......................");
+  console.log(cdp);
+  newVenta.total = newVenta.mTotal;
+  newVenta.mTotal = newVenta.mTotal - newVenta.adelanto;
+ console.log("codigos productos  ");
+ console.log(cdp);
+  const cliente = await client.findOne({dni: newVenta.dni});
+  newVenta.geolocalizacion = cliente.geolocalizacion;
+  const anio = new Date(fecha).getFullYear();
+  const mes = new Date(fecha).getMonth();
+  const dia = new Date(fecha).getUTCDate();
+  const fechaAc = new Date(anio, mes, (dia + 1)).toLocaleDateString("es-AR", {timeZone: 'America/Argentina/Buenos_Aires'});
+  var fechaV  = new Date(anio, mes, dia);//.toDateString("es-AR", {timeZone: 'America/Argentina/Buenos_Aires'});
+  var fechaVencimiento = "";
+  if (newVenta.plan === "diario") {
+    var DiaDom = parseInt(newVenta.cuotas/6);
+    fechaVencimiento = new Date(fechaV.setDate(fechaV.getDate() + (newVenta.cuotas + DiaDom)));
+  } else {
+    fechaVencimiento = new Date(fechaV.setDate(fechaV.getDate() + (newVenta.cuotas * 7)));
+  }
+  newVenta.timeStamp =  new Date(anio, mes, dia).toDateString("es-AR", {timeZone: 'America/Argentina/Buenos_Aires'});
+  newVenta.fechaInicio = fechaAc;
+  newVenta.fechaFinal = fechaVencimiento.toLocaleDateString("es-AR", {timeZone: 'America/Argentina/Buenos_Aires'});
+  newVenta.DateFinal= fechaVencimiento;
+  var montGanancia = newVenta.total - newVenta.monto;
+  const balan = await balance.findOne({cobRuta: newVenta.cobRuta, fecha: fechaAc});
+  balan.ventas = balan.ventas + newVenta.monto;
+  balan.ganancia = balan.ganancia + montGanancia;
+  await balance.findByIdAndUpdate(balan._id, balan);
+  await newVenta.save();
+  
+  if (newVenta.venRuta !== 1) { 
+    if (newVenta.categoria == "prestamo") {
+      newVenta.monto = newVenta.monto;
+    }
+    const newHistoryVenta = new historyVentas({
+      nombre: newVenta.nombre,
+      dni: newVenta.dni,
+      venRuta: newVenta.venRuta,
+      cobRuta: newVenta.cobRuta,
+      codProd: newVenta.codProd,
+       user: nRuta,
+      detalle: newVenta.detalle,
+      mTotal: newVenta.total,
+      descuento: newVenta.descuento,
+      categoria: newVenta.categoria,
+      plan: `${newVenta.plan} / Cuotas: ${newVenta.cuotas} / Cuota: ${newVenta.cuota}`,
+      fechaInicio: fechaAc
+    });
+    await newHistoryVenta.save();
+  }
+  //operacion para sumar el ingreso de un adelanto cuando es venta de productos
+ //....................................................
+  
+  if (ade > 0) {
+    console.log("dentro delif adelantos");
+    
+    switch (rol) {
+        case "pisoDeVenta":
+          const balan = await balance.findOne({cobRuta: nRuta, fecha: fechaAc});
+        const Tt = (((balan.vtaCtdo)*1) + ((newVenta.adelanto)*1)).toFixed(2);
+        balan.vtaCtdo = Tt;
+       const newBalan = await balance.findByIdAndUpdate({_id: balan._id}, balan);
+          break;
+         case "admin":
+              console.log("dentro delif adelantos / case");
+            const newCaja = new cajaOp({monto: newVenta.adelanto, fecha: fechaAc, userCod: nRuta, tipo: "ingreso", detalle: newVenta.detalle, timeStamp: new Date()});
+            await newCaja.save();
+          break;
+        default:
+          break;
+       }
+  }
+  //operacion restar stock de productos
+ //....................................................
+  var prod= 0;
+  if (Array.isArray(cdp)) {
+    for (let i = 0; i < cdp.length; i++) {
+      const element = cdp[i];
+      prod = await product.findOne({cod: element});
+     var cantProd = req.body[element];
+
+      prod.stock = prod.stock - cantProd;
+      await product.findByIdAndUpdate({_id: prod._id}, prod);
+    }
+  } else if (cdp != 'prestamo') { 
+      prod = await product.findOne({cod: cdp});
+      var cantProd = req.body[cdp];
+      prod.stock = prod.stock - cantProd;
+      await product.findByIdAndUpdate({_id: prod._id}, prod); 
+    } 
+  
+  res.redirect('/ventas');
+  } catch (error) {
+    const mensajeError = error;
+    res.render('error', {mensajeError});
+}
+};
+
+const preCotizarCont = async(req, res) =>{
+  try {
+    const cot = req.body;
+    const codProducts = cot.codProd;
+    const cliente = await client.findOne({dni:cot.dni});
+     const arrayCod = [];
+     if (Array.isArray(codProducts)) {
+          for (let i = 0; i < codProducts.length; i++) {
+            const element = codProducts[i];
+            console.log(element);
+            
+            var prod = await product.findOne({cod: element});
+            arrayCod.push({"cod":element,"nombre": prod.nombre});
+          };  
+          res.render('cantProdVentCont', {cot,arrayCod, cliente});
+        }
+        else{
+          var prod = await product.findOne({cod: codProducts});
+          arrayCod.push({"cod":codProducts, "nombre": prod.nombre});
+          precio= prod.precio;
+          res.render('cantProdVentCont', {cot,arrayCod, cliente});
+        }
+
+  } catch (error) {
+    
+  }
+} 
+
+const cotizarContado = async(req, res)=>{
+try {
+   const cot = req.body;
+    const cliente = await client.findOne({dni:cot.dni});
+   const codProducts = cot.codProd;
+   const valores = await setValores.findOne();
+   var precio = 0;
+   const arrayCod = [];
+   if (Array.isArray(codProducts)) {
+    console.log("array");
+    
+          for (let i = 0; i < codProducts.length; i++) {
+            const element = codProducts[i];
+            var prod = await product.findOne({cod: element}); 
+            const cantProd = req.body[element];
+            precio = (prod.precio * cantProd) + precio;
+            console.log(arrayCod);
+            
+            arrayCod.push({"cod":element, "nombre": prod.nombre, "precio": prod.precio, "cant": cantProd, "Total": (prod.precio * cantProd) });
+          }
+        }
+        else{
+          var prod = await product.findOne({cod: codProducts});
+          const cantProd = req.body[codProducts];
+          precio= prod.precio * cantProd;
+          arrayCod.push({"cod":codProducts, "nombre": prod.nombre, "precio": prod.precio, "cant": cantProd, "Total": (prod.precio * cantProd) });
+        }
+        console.log("corizar contado");
+        
+        const precioT = (precio * valores.dolar) * ((valores.porcentaje / 100) +1);
+        const des = (cot.descuento / 100);
+        const precioTotal = (precioT - (des * precioT)).toFixed(2);
+     
+   
+        console.log("precio: " + precioTotal);
+       
+   res.render('confirmarVenContado', {precioTotal, cliente, cot, arrayCod});
+} catch (error) {
+  var mensajeError = "Seleccione un producto"
+   res.render("error", {mensajeError})
+}
+};
+
+const guardarVentasContado = async(req, res)=>{
+  try {
+    const vent = req.body;
+    const m = ((vent.mT)*1).toFixed(2);
+    const cdp = vent.codProd;
+   var detalle = "";
+  const fechaAc = new Date().toLocaleDateString("es-AR", {timeZone: 'America/Argentina/Buenos_Aires'});
+  const valores = await setValores.findOne();
+
+    const token =  req.cookies.token; // Obtener el token JWT de las cookies de la solicitud
+    const verifyToken = await verifyJWT(token);
+    const rol = verifyToken.role;
+    const nRuta = verifyToken.numRuta;
+     var cantProd = 0;
+     var arrayProdBoleta = [];
+     var totalTo = 0;
+      if (Array.isArray(cdp)) {
+      for (let i = 0; i < cdp.length; i++) {
+        const element = cdp[i];
+        cantProd = req.body[element];
+        var prod = await product.findOne({cod: element});
+        const precioT = (prod.precio * valores.dolar) * ((valores.porcentaje / 100) +1);
+        prod.stock = prod.stock - cantProd;
+        var precio = f.format(precioT);
+        var total= f.format((precioT * cantProd));
+        await product.findByIdAndUpdate({_id: prod._id}, prod);
+        totalTo = totalTo + (precioT * cantProd);
+        arrayProdBoleta.push({"cod":element,"nombre": prod.nombre,"garantia":prod.garantia ,"precio": precio, "cant": cantProd, "total": (total) });
+        detalle = detalle + "Cant: " + cantProd + ", "+ prod.nombre + ", Cod: " + prod.cod + " || ";
+        
+      } } else { 
+        var prod = await product.findOne({cod: cdp});
+        cantProd = req.body[cdp];
+        const precioT = (prod.precio * valores.dolar) * ((valores.porcentaje / 100) +1);
+        prod.stock = prod.stock - cantProd;
+        var precio = f.format(precioT);
+        var total= f.format((precioT * cantProd));
+        prod.stock = prod.stock - cantProd;
+        arrayProdBoleta.push({"cod":cdp,"nombre": prod.nombre,"garantia":prod.garantia ,"precio": precio, "cant": cantProd, "total": total});
+         detalle = "Cant: " + cantProd + ", "+ prod.nombre + ", Cod: " + prod.cod + " || ";
+         totalTo = (precioT * cantProd);
+        console.log("new prod");
+        await product.findByIdAndUpdate({_id: prod._id}, prod); 
+      } 
+      
+      const newHistoryVenta = new historyVentas({
+        nombre: vent.nombre,
+        dni: vent.dni,
+        venRuta: vent.venRuta,
+        codProd: vent.codProd,
+        detalle: detalle,
+        mTotal: m,
+        descuento: vent.descuento,
+        categoria: "contado",
+        plan: vent.plan,
+        fechaInicio: fechaAc,
+        user: nRuta
+      });
+      await newHistoryVenta.save();
+      
+      switch (rol) {
+   
+            case "pisoDeVenta":
+           const balan = await balance.findOne({cobRuta: nRuta}).sort({timeStamp: -1});
+           console.log("balance encontrado:" + balan);
+           
+         const Tt = (((balan.vtaCtdo)*1) + ((vent.mT)*1)).toFixed(2);
+         balan.vtaCtdo = Tt;
+        const newBalan = await balance.findByIdAndUpdate({_id: balan._id}, balan);
+           break;
+        case "admin":
+           const newCaja = new cajaOp({monto: m, fecha: fechaAc, userCod: nRuta, tipo: "ingreso", detalle: detalle, timeStamp: new Date()});
+           await newCaja.save();
+         break;
+       default:
+         break;
+      }
+      var subTotal = f.format(totalTo);
+      var des = (vent.descuento / 100) * totalTo;
+      var totalT = f.format(totalTo - des);
+      des = f.format(des);
+     console.log("array productos boleta");
+     console.log(newHistoryVenta);
+    res.render('boleta', {newHistoryVenta,fechaAc, totalT, subTotal, arrayProdBoleta, des});
+  } catch (error) {
+    var mensajeError = error + "Error al guardar la venta";
+    res.render('error', {mensajeError});
+  }
+};
+
+
+
+
+
+module.exports = {preCotizar, fecthVentasMes, preCotizarCont, cotizarPlan, cotizarContado, cargarVentas, guardarVentasContado, guardarVentas, cargarVenCob};
