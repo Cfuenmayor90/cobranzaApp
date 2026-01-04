4c89fee86e0f6e5f5324240df8a005698b24b6b5
Cfuenmayor90 <137357380+Cfuenmayor90@users.noreply.github.com>
2026-01-03 22:36:36 -0300

    Add files via upload

---
+++ controllers/productsRoutes.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+const {checkRole} = require('../controllers/usersControllers');
+const {cargarProducts, imprimirProd, cargarPagProductos, upload, saveProducts, cotizarProd, filtrarProd, prodEditGet, prodEditSave, prodDelete, habladores} = require('../controllers/productControllers');
+
+router.get('/productosUsuarios', cargarPagProductos);
+
+router.get('/productos', checkRole(['admin']), cargarProducts);
+
+router.get('/imprimirProd', checkRole(['admin']), imprimirProd);
+
+router.get('/filtrarProd/:categoria', filtrarProd);
+ 
+router.post('/saveProduct', checkRole(['admin']), upload, saveProducts);
+
+router.get('/cotizarProd/:id', cotizarProd);
+
+router.get('/prodEdit/:_id', checkRole(['admin']), prodEditGet);
+
+router.post('/prodEditSave/:id', checkRole(['admin']), upload, prodEditSave);
+
+router.get('/delete/:id', checkRole(['admin']), prodDelete);
+
+router.get('/habladores', checkRole(['admin']), habladores);
+
+module.exports = router;
